cbd44ee0a846f2cf97744a91f5099fff3e808339
Alisson <alissonalves64@hotmail.com>
2023-09-18 19:43:06 -0300

    feat: Estilização do header  e lógica para nav

---
+++ src/Components/Header.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import styles from "./Header.module.css";
+import LogoutIcon from "@mui/icons-material/Logout";
+import { IconButton } from "@mui/material";
+import { signOut } from "firebase/auth";
+
+import { useNavigate } from "react-router-dom";
+import { auth } from "../services/firebase";
+
+
+const Header = () => {
+  const navigate = useNavigate();
+  const [user, setUser] = React.useState(null);
+
+  React.useEffect(() => {
+    if (localStorage.getItem("@detailUser")) {
+      const userData = JSON.parse(localStorage.getItem("@detailUser"));
+      setUser(userData);
+    }
+  }, []);
+
+  async function handleLogout() {
+    signOut(auth)
+      .then(() => {
+        localStorage.removeItem("@detailUser");
+        setUser(null);
+        navigate("/");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  return (
+    <header className={styles.header}>
+      <img className={styles.logo} src="./public/assets/taugor.png" alt="Taugor-home-logo" />
+      <div>
+        {user && (
+          <div>
+            <span className={styles.email}> {user.email}</span>
+            <IconButton
+              sx={{ mr: "2rem" }}
+              onClick={handleLogout}
+              aria-label="logout"
+              size="large"
+            >
+              <LogoutIcon fontSize="inherit" />
+            </IconButton>
+          </div>
+        )}
+      </div>
+    </header>
+  );
+};
+
+export default Header;
