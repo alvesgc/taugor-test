06fd30a867db97df4abd85d37c52474442baa83b
Alisson <alissonalves64@hotmail.com>
2023-09-18 19:40:44 -0300

    add: Header styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./Home";
+import Home from "./Home.jsx";
+import Header from "./Components/Header.jsx"
 
 function App() {
   return (
     <div className="container">
       <BrowserRouter>
+      <Header />
        <Routes>
         <Route path = "/" element = {<Home />} /> 
        </Routes>
